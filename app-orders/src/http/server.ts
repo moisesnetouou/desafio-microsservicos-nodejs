8e80424a7494e465c05e363afc7a8580cf86f2c8
Moisés <moisesnetored@gmail.com>
2025-06-28 22:47:34 -0400

    feat: add customers schema and save into database orders

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,8 +1,11 @@
 import { fastify } from "fastify"
 import { fastifyCors } from "@fastify/cors"
 import { z } from "zod"
+import { randomUUID } from "node:crypto"
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod"
 import { channels } from "../broker/channels/index.ts"
+import { db } from "../db/client.ts"
+import { schema } from "../db/schema/index.ts"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -27,7 +30,18 @@ app.post("/orders", {
   const { amount } = request.body
 
   console.log('Creating an order with amount', amount)
+
   channels.orders.sendToQueue("orders", Buffer.from("Hello World"))
+
+  try {
+    await db.insert(schema.orders).values({
+      id: randomUUID(),
+      customerId: '3e6f5421-c79e-41a9-b888-d4c9fb203262',
+      amount
+    })
+  } catch (err) {
+    console.log(err)
+  }
 
   return reply.status(201).send()
 })
